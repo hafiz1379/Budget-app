cb068e144ab356d88d960db50da1a45ac332f957
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-21 13:59:32 +0430

    Add models for Category, Payment, PaymentCategory, and User

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  has_many :categories
+  has_many :payments
+
+  validates :name, presence: true
+end
