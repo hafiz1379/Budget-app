b64e7c8b347a07baff9b4a6472bb84d8e6d66059
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-21 14:20:11 +0430

    Update routes.rb file to include payments and categories resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'payments/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_for :users
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'welcome_pages#index'
+
+  resources :categories, only: [:index] do
+    resources :payments, only: [:index]
+  end
+
 end
